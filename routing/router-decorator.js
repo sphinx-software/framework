77efe1db1f57a99b51ec1dccc6f9f09a7b247677
sonle <rikky@sphinx-software.com>
2017-04-04 13:48:55 +0700

    add routing decorator service

--- routing/router-decorator.js
+++ routing/router-decorator.js
@@ -1,14 +1,31 @@
+const lodash = require('lodash');
+
 class RouterDecorator {
 
-    constructor(container) {
-        this.container = container;
+    constructor(controllerHandlerResolver) {
+        this.controllerHandlerResolver = controllerHandlerResolver;
     }
 
-    decorate(router, routeSpec) {
+    decorate(config, router) {
+        lodash.each(config, (routeConfig, routeName) => {
 
+            let handlers = routeConfig.handlers.map((spec) => {
+                if (lodash.isString(spec)) {
+                    return this.controllerHandlerResolver.resolve(spec)
+                }
+                return spec;
+            });
+
+            router[routeConfig.method](routeName, routeConfig.url, ...handlers)
+        });
     }
-
-    resolveRouteHandler(routeSpec) {
-    }
-
 }
+
+module.exports = RouterDecorator;
+//
+// {
+//     hello: {
+//         method: 'get',
+//         action: [fada, ,fasdf,]
+//     }
+// }
--- routing/routing.provider.js
+++ routing/routing.provider.js
@@ -1,5 +1,13 @@
-exports.register = () => {};
+const RouterDecorator = require('./router-decorator');
+const ControllerResolver = require('./controller-handler-resolver');
+
+exports.register = (container) => {
+    container.singleton('http.router.decorator', async () => new RouterDecorator(new ControllerResolver(container)))
+};
 
 exports.boot = async (container) => {
-    // todo
+    let routerDecorator = await container.make('http.router.decorator');
+    let config          = await container.make('config');
+
+    routerDecorator.decorate(config.routes, await container.make('http.router'));
 };
