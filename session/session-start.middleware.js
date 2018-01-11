0d0e049e4068db39e0d543dd89291398a52cd174
NgocTo <tongoc92@gmail.com>
2018-01-11 17:46:08 +0700

    emit session.started event when session start

--- session/session-start.middleware.js
+++ session/session-start.middleware.js
@@ -6,6 +6,7 @@ module.exports = async (context, next) => {
     let config          = await container.make('config');
     let sessionStorage  = await container.make('session.storage');
     let sessionID       = context.cookies.get('sphinx-session-id') || uuid();
+    let event           = await container.make('events');
 
     // Get the session with the given sessionId.
     // if no session was found, then give it a new one
@@ -20,12 +21,22 @@ module.exports = async (context, next) => {
         context.session = new Session(await container.make('serializer'));
     }
 
+    // emit event when session started
+    // other service aware to session will updated
+    event.emit('session.started', context.session);
+
     await next();
 
 
     if (context.session.shouldDestroy()) {
+        // emit event when session starting
+        event.emit('session.destroying', context.session);
+
         // In case the session was set destroy flag explicity
         await sessionStorage.unset(sessionID);
+
+        // emit event when session starting
+        event.emit('session.destroyed', context.session);
     } else {
         // Touch the session active timestamp then
         // Store back the session data at the end of the request life cycle
