6537f2034795236f246b155d6f09ddbd9d95d03b
rikky <son.levuthai@gmail.com>
2018-01-26 15:21:09 +0700

    rewrite log service

--- test-skeleton/config/index.js
+++ test-skeleton/config/index.js
@@ -1,9 +1,14 @@
+import 'winston-daily-rotate-file';
+import winston from 'winston';
+import path    from 'path';
+
 export default  {
 
     modules: [
         // Frameworks Module
         './../Http',
         './../Hash',
+        './../Log',
         './../MetaInjector',
         './../storage',
 
@@ -20,5 +25,28 @@ export default  {
 
     hash: {
         rounds: 10
+    },
+
+    log: {
+        transports: [
+
+            // File rotate logger
+            new winston.transports.DailyRotateFile({
+                filename: path.normalize(`${__dirname}/../storages/logs/sphinx--`),
+                datePattern: 'yyyy-MM-dd.log',
+                level: 'debug'
+            }),
+
+            // Error logger
+            new winston.transports.File({
+                filename: path.normalize(`${__dirname}/../storages/logs/error.log`),
+                level: 'error'
+            }),
+        ],
+
+        level: process.env.LOG_LEVEL || 'debug'
     }
 };
+
+
+
