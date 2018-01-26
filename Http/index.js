4e1cf8ab840c1851e9cde15c31eb722a48288060
rikky <son.levuthai@gmail.com>
2018-01-26 14:22:33 +0700

    controller & routing

--- Http/index.js
+++ Http/index.js
@@ -19,18 +19,12 @@ export class HttpServiceProvider {
                 await next();
             });
         });
-
-        this.container.singleton('http.router', async () => {
-            let config = await this.container.make('config');
-
-            return new Router(config.http.router);
-        });
     }
 
     async boot() {
         let kernel = await this.container.make('http.kernel');
 
-        let middlewares = this.fusion.getByManifest('kernelMiddleware');
+        let middlewares = this.fusion.getByManifest('http.kernelMiddleware');
 
         for (let index = 0; index < middlewares.length; index++) {
             const Symbol   = middlewares[index];
@@ -42,6 +36,89 @@ export class HttpServiceProvider {
     }
 }
 
+@provider()
+export class HttpRouterServiceProvider {
+    constructor(container, fusion) {
+        this.container = container;
+        this.fusion    = fusion;
+    }
+
+    register() {
+        this.container.singleton('http.router', async () => {
+            let config = await this.container.make('config');
+
+            return new Router(config.http.router);
+        });
+    }
+
+    async boot() {
+        let controllers = this.fusion.getByManifest('http.controller');
+        let router      = await this.container.make('http.router');
+
+        for (let index = 0; index < controllers.length; index++) {
+            await this.bindController(router, controllers[index]);
+        }
+    }
+
+    async bindController(router, Controller) {
+        let controller = await this.container.make(Controller);
+
+        let controllerActionNames = Object.getOwnPropertyNames(Controller.prototype)
+            .filter(methodName => Reflect.hasMetadata('http.action', controller, methodName));
+
+        controllerActionNames.forEach(actionName => {
+            let metadata = Reflect.getMetadata('http.action', controller, actionName);
+
+            router[metadata.method](`${Controller.name}@${actionName}`, metadata.url, controller[actionName]);
+        })
+
+    }
+}
+
 export function kernelMiddleware(value) {
-    return Reflect.metadata('kernelMiddleware', value)
+    return Reflect.metadata('http.kernelMiddleware', value);
+}
+
+export function controller(prefix = '') {
+    return Reflect.metadata('http.controller', prefix);
+}
+
+export function action(method, url, middlewares = []) {
+    return Reflect.metadata('http.action', {
+        method      : method,
+        url         : url,
+        middlewares : middlewares
+    });
+}
+
+export function get(url, middlewares = []) {
+    return Reflect.metadata('http.action', {
+        method      : 'get',
+        url         : url,
+        middlewares : middlewares
+    });
+}
+
+export function post(url, middlewares = []) {
+    return Reflect.metadata('http.action', {
+        method      : 'post',
+        url         : url,
+        middlewares : middlewares
+    });
+}
+
+export function put(url, middlewares = []) {
+    return Reflect.metadata('http.action', {
+        method      : 'put',
+        url         : url,
+        middlewares : middlewares
+    });
+}
+
+export function del(url, middlewares = []) {
+    return Reflect.metadata('http.action', {
+        method      : 'del',
+        url         : url,
+        middlewares : middlewares
+    });
 }
--- test-skeleton/src/Http/index.js
+++ test-skeleton/src/Http/index.js
@@ -1,4 +1,4 @@
-import {kernelMiddleware} from "../../../Http/index";
+import {kernelMiddleware, controller, get} from "../../../Http";
 import {singleton} from "../../../MetaInjector";
 
 @singleton()
@@ -8,5 +8,20 @@ export class HelloMiddleware {
     async handle(context, next) {
         context.body = {hello: 'world'};
         await next();
+    }
+}
+
+
+@singleton('config')
+@controller()
+export class WelcomeController {
+    constructor(config) {
+        this.config = config;
+    }
+
+    @get('/hello')
+    async foo(context) {
+        //
+        context.body = {foo: 'bar2'}
     }
 }
