5b8e3a3e7f7d3708cc5504c3d478627b7b3d3806
rikky <son.levuthai@gmail.com>
2018-01-26 18:07:55 +0700

    purge test-skeleton

--- test-skeleton/config/cache.js
+++
@@ -1,9 +0,0 @@
-import path from 'path';
-
-export default {
-
-    adapter  : 'filesystem',
-    prefix   : 'cache',
-    directory: path.normalize(path.join(__dirname, '..', 'storages', 'caches'))
-
-};
