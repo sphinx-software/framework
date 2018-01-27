7c03fdaeae6953738d0af6a83d81e18a1a02ad9b
rikky <son.levuthai@gmail.com>
2018-01-27 10:06:31 +0700

    add view macros

--- View/ViewServiceProvider.js
+++ View/ViewServiceProvider.js
@@ -4,8 +4,9 @@ import {provider} from "../Fusion/Fusion";
 
 @provider()
 export default class ViewServiceProvider {
-    constructor(container) {
+    constructor(container, fusion) {
         this.container = container;
+        this.fusion    = fusion;
     }
 
     register() {
@@ -14,4 +15,32 @@ export default class ViewServiceProvider {
             return new ViewFactory(new EventEmitter(), engine);
         });
     }
+
+    async boot() {
+        let viewFactory = this.container.make('view');
+
+        let viewRenderingMacros = this.fusion.getByManifest('view.decorator.rendering');
+
+        viewRenderingMacros.map(async Macro => {
+            let templateName = Reflect.getMetadata('view.decorator.rendering', Macro);
+            let macro        = await this.container.make(Macro);
+
+            viewFactory.rendering(templateName, (...arguments) => macro.run(arguments));
+        });
+
+        viewRenderingMacros.map(async Macro => {
+            let templateName = Reflect.getMetadata('view.decorator.making', Macro);
+            let macro        = await this.container.make(Macro);
+
+            viewFactory.making(templateName, (...arguments) => macro.run(arguments));
+        });
+    }
+}
+
+export function viewRendering(templateName) {
+    return Reflect.metadata('view.decorator.rendering', templateName);
+}
+
+export function viewMaking(templateName) {
+    return Reflect.metadata('view.decorator.making', templateName);
 }
--- View/index.js
+++ View/index.js
@@ -1,5 +1,7 @@
-export {ViewEngineNunjucksServiceProvider} from "./NunjucksEngine";
+export {viewRendering, viewMaking} from "./ViewServiceProvider";
 export ViewServiceProvider from "./ViewServiceProvider";
 export ViewRenderMiddleware from "./ViewRenderMiddleware";
 export ViewFactory from "./ViewFactory";
 export View from "./View";
+
+export {ViewEngineNunjucksServiceProvider} from "./NunjucksEngine";
