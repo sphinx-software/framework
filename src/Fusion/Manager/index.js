5e5acdfc6c2437c5deb91e2bed6ab8c35fa30de6
sieunhangao <friendd1996@gmail.com>
2018-02-23 10:29:57 +0700

    :musical_keyboard: update manager

--- src/Fusion/Manager/index.js
+++ src/Fusion/Manager/index.js
@@ -21,6 +21,13 @@ export default class Manager {
     defaultAdapter = null;
 
     /**
+     * List adapters factories will resolved
+     *
+     * @type {{ driver: String }}
+     */
+    configAdapters = {};
+
+    /**
      * List of factories method which will provides the adapter
      *
      * @type {{}}
@@ -39,14 +46,24 @@ export default class Manager {
     }
 
     /**
-     * Extend this manager by registering an adapter factory
      *
-     * @param {string} adapterName
-     * @param {Function} adapterFactory
+     * @param {{ driver: String }} configAdapters
      * @return {Manager}
      */
-    extend(adapterName, adapterFactory) {
-        this.factories[adapterName] = adapterFactory;
+    setConfigAdapters(configAdapters) {
+        this.configAdapters = configAdapters;
+        return this;
+    }
+
+    /**
+     * Extend this manager by registering an adapter factory
+     *
+     * @param {string} driver
+     * @param {Function} driverFactory
+     * @return {Manager}
+     */
+    extend(driver, driverFactory) {
+        this.factories[driver] = driverFactory;
         return this;
     }
 
@@ -70,11 +87,7 @@ export default class Manager {
      * @param {string|null} adapterName
      * @return {*}
      */
-    adapter(adapterName = null) {
-
-        if (!adapterName) {
-            adapterName = this.defaultAdapter;
-        }
+    adapter(adapterName = this.defaultAdapter) {
 
         // If adapterName still a falsy value, consider as an error
         if (!adapterName) {
@@ -88,17 +101,17 @@ export default class Manager {
 
         // Otherwise, if the extended list doesn't have such adapter name
         // consider as adapter not supported
-        if(!this.factories[adapterName]) {
+        if (!this.configAdapters[adapterName]) {
             throw new VError(`E_MANAGER: Adapter [${adapterName}] is not supported`);
         }
 
         let factoryFunction = lodash.isFunction(this.factories[adapterName])
-            ? this.factories[adapterName] : () => this.factories[adapterName].make()
+            ? this.factories[adapterName] : this.factories[adapterName].make
         ;
 
         // If we have the adapter factory in the extend list,
         // we'll resolve the adapter and also cached it.
-        this.adapters[adapterName] = factoryFunction();
+        this.adapters[adapterName] = factoryFunction(this.configAdapters[adapterName]);
 
         return this.adapters[adapterName];
     }
