3e1883c31949f4597c308ee9876d2fd7fcd6082a
NgocTo <tongoc92@gmail.com>
2018-03-05 14:26:48 +0700

    integrate unit test for BcryptHasher

--- test/Test/index.js
+++ test/Test/index.js
@@ -1,5 +1,6 @@
 export AuthTestSuite from "./AuthTestSuite";
 export SessionTestSuite from "./SessionTestSuite";
 export SerializerTestSuite from "./SerializerTestSuite";
+export BcryptHasherTestSuite from "./BcryptHasherTestSuite"
 export * from './View/testFlashBag';
 export * from "./Storage"
